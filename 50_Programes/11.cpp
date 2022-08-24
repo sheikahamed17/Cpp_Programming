721620b002471f81a4d4a2129e2b4f9d866af68c
Sheik Ahamed <sdsheikahamed@gmail.com>
2022-08-24 06:36:35 +0530

    Added another 2 C++ Programes

---
+++ 50_Programes/11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int num;
+    cout<<"Enter a unsigned int: ";
+    cin>>num;
+    cout<<"The unsigned int is "<<num;
+}
+
---
+++ 50_Programes/12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a =10,b=7;
+    cout<<"a++ = "<<a++<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b-- = "<<b--<<endl;
+    cout<<"b = "<<b<<endl;
+    cout<<"++a = "<<++a<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"--b = "<<--b<<endl;
+    cout<<"b = "<<b<<endl;
+}
+
