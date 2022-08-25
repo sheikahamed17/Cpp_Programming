684b85441a71c9845b5e9b05c50ffbb7f55ffbe4
Sheik Ahamed <sdsheikahamed@gmail.com>
2022-08-25 14:01:41 +0530

    Added one C++ Program

---
+++ 50_Programes/13.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10,b = 20;
+    cout<<"A+B(Addition) = "<<a+b<<endl;
+    cout<<"A-B(Subraction) = "<<a-b<<endl;
+    cout<<"A*B(Multiplication) = "<<a*b<<endl;
+    cout<<"A/B(Division) = "<<a/b<<endl;
+    cout<<"A%B(Modulus) = "<<a%b<<endl;
+    return 0;
+}
